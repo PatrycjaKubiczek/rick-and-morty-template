b6c2cb25a60538a99ac09caee0adb83e681f095e
Patrycja Kubiczek <p.kubiczek@gmail.com>
2022-08-06 17:25:43 +0200

    set favourite character

--- src/CharacterPage.js
+++ src/CharacterPage.js
@@ -5,12 +5,14 @@ import {
     AccordionItem,
     AccordionPanel,
     Box,
+    Button,
     Container
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
+import { StarIcon } from '@chakra-ui/icons';
 
 const Character = () => {
     let navigate = useNavigate();
@@ -19,6 +21,20 @@ const Character = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const [isFavorite, setFavourite] = useState(false);
+
+    const checkIfFavourite = (id) => {
+        const favouritesInStorage = localStorage.getItem('Favourites');
+        if (favouritesInStorage) {
+            const favouritesArr = JSON.parse(favouritesInStorage);
+            if (favouritesArr.includes(id)) {
+                setFavourite(true);
+            }
+            return;
+        }
+        setFavourite(false);
+        return false;
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,17 +56,56 @@ const Character = () => {
         };
         fetchData();
     }, [params.characterId]);
+
+    useEffect(() => {
+        checkIfFavourite(data.id);
+    }, [data.id]);
+
     if (loading) {
         return <p>Loading...</p>;
     }
     if (error) {
         return <p>{errorMessage}</p>;
     }
+    const addToFavorite = (id) => {
+        let favs = JSON.parse(localStorage.getItem('Favourites')) || [];
+        favs.push(id);
+        localStorage.setItem('Favourites', JSON.stringify(favs));
+        setFavourite(true);
+    };
+    const removeFromFavourites = (id) => {
+        const favs = JSON.parse(localStorage['Favourites']);
+        if (favs) {
+            const newFavs = favs.filter((fav) => fav !== id);
+            localStorage.setItem('Favourites', JSON.stringify(newFavs));
+            setFavourite(false);
+        }
+    };
 
     return (
         <>
             <Navbar />
             <Container maxW="md">
+                {isFavorite ? (
+                    <Button
+                        rightIcon={<StarIcon />}
+                        colorScheme="teal"
+                        variant="outline"
+                        onClick={() => removeFromFavourites(data.id)}
+                    >
+                        Remove from favorites
+                    </Button>
+                ) : (
+                    <Button
+                        rightIcon={<StarIcon />}
+                        colorScheme="teal"
+                        variant="solid"
+                        onClick={() => addToFavorite(data.id)}
+                    >
+                        Add to favorites
+                    </Button>
+                )}
+
                 <img src={data.image} alt={data.name} />
                 <h1>{data.name}</h1>
                 <p>{data.status}</p>
